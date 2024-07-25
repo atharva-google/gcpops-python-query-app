05d4a4306c112e12a348ead5299516270cba85db
atharvachalke <atharvachalke@google.com>
2024-07-25 13:22:00 +0530

    Prompt Changes

--- app.py
+++ app.py
@@ -20,42 +20,48 @@ class CustomEmbeddingFunction(EmbeddingFunction[Documents]):
     def __call__(self, input: Documents) -> Embeddings:
         return EMBED_MODEL.encode(input).tolist()
 
+@st.cache_data
+def initialize_formulas():
+    NOW = datetime.datetime.now()
+    YEAR = NOW.year
+    QUARTER = math.ceil(NOW.month / 3)
+
+    START_DATE = "Jan 2023"
+    END_DATE = "Dec 2024"
+
+    formula_map = {
+        "Promotion (Promo) Credits": "Gross revenue - Net revenue",
+
+        "Daily Run Rate (DRR)": "Last 7 days revenue / 7 OR Last 14 days revenue / 14 OR Any month revenue / Number of days in the month",
+
+        "Monthly Run Rate (MRR)": "Last 90 days revenue / 3 ",
+        "Monthly Run Rate (MRR) of a requested `MONTH`": "Last 3 months revenue from `MONTH` / 3 (eg: March 2024 MRR = ([january 2024] + [february 2024] + [march 2024]) / 3)",
+
+        "Annual Run Rate (ARR)": f"total {YEAR}-Q{QUARTER-1} revenue * 4",
+        "Incremental Run Rate (IRR)": f"total {YEAR}-Q{QUARTER-1} revenue * 4 - total {YEAR-1}-Q4 revenue * 4",
+
+        "Criteria to get accounts that started billing/revenue IN the last `N` days": f"([Total revenue from {START_DATE} to {END_DATE}] - [current last `N` days revenue]) <= 0 AND [current last `N` days revenue] > 0",
+        "Criteria to get accounts that started billing/revenue IN or DURING a `MONTH`": f"([Total revenue from {START_DATE} to `MONTH`]) <= 0 AND [`MONTH` revenue] > 0",
+        "Criteria to get accounts that started billing/revenue AFTER a `MONTH`": f"([Total Revenue from {START_DATE} to `MONTH`]) <= 0 AND [Total Revenue from requested `MONTH` to {END_DATE}] > 0"
+    }
+
+    return formula_map
+
 @st.cache_resource
 def initialize():
     client = bigquery.Client(project=PROJECT_ID)
-
     df = client.query(f"SELECT * FROM {TABLE_ID}").to_dataframe()
 
-    table_ref = client.get_table(TABLE_ID)
-    data_schema = "\n".join([f"\t{str(field.name)}: {str(field.description)}" for field in table_ref.schema])
-
-    formula_map = {
-        "Promotion (Promo) Credits": "Gross Revenue - Net Revenue",
-        "Daily Run Rate (DRR)": "Last 7 days revenue / 7 | Last 14 days revenue / 14 | [Any month revenue] / [Number of days in the month]",
-        "Monthly Run Rate (MRR)": "[Last 90 days data] / 3 | [Last 3 data months from requested month] / 3 (eg: March 2024 MRR = ([january 2024 revenue] + [february 2024 revenue] + [march 2024 revenue]) / 3)",
-        "Annual Run Rate (ARR)": "[PREVIOUS QUARTER] * 4",
-        "Incremental Run Rate (IRR)": "[PREVIOUS QUARTER] * 4 - [last year Quarter 4] * 4",
-        "Criteria to get accounts that started billing/revenue IN the last `N` days": "([Total revenue from STARTING DATE to ENDING DATE] - [last `N` days revenue from current date]) <= 0 AND [last `N` days revenue from current date] > 0",
-        "Criteria to get accounts that started billing/revenue IN or DURING a `MONTH`": "([Total revenue from STARTING DATE to `MONTH`]) <= 0 AND [requested month revenue] > 0",
-        "Criteria to get accounts that started billing/revenue AFTER a `MONTH`": "([Total Revenue from STARTING DATE to `MONTH`]) <= 0 AND [Total Revenue from requested month to ENDING DATE] > 0"
-    }
+    # table_ref = client.get_table(TABLE_ID)
+    # data_schema = "\n".join([f"\t- {str(field.name)}: {str(field.description)}" for field in table_ref.schema])
 
     embed_model = SentenceTransformer("./all-MiniLM-L6-v2")
 
     chroma_client = chromadb.PersistentClient(path=CHROMA_PATH)
     chroma_db = chroma_client.get_collection(name=CHROMA_NAME, embedding_function=CustomEmbeddingFunction())
 
-    return df, data_schema, formula_map, embed_model, chroma_db
-
-@st.cache_data
-def initialize_date():
-    now = datetime.datetime.now()
-    DAY = now.day
-    MONTH = now.strftime("%B")
-    YEAR = now.year
-    QUARTER = f"Q{math.ceil(now.month / 3)}"
-    return DAY, MONTH, YEAR, QUARTER
-    
+    return df, embed_model, chroma_db
+   
 LOCATION = "us-central1"
 PROJECT_ID = "gcpops-427012"
 MODEL_NAME = "gemini-1.5-flash-001"
@@ -63,53 +69,63 @@ MODEL_NAME = "gemini-1.5-flash-001"
 DATASET_ID = f"{PROJECT_ID}.gcp_core"
 TABLE_ID = f"{DATASET_ID}.revenue"
 
+N_FORMULAS = 3
 CHROMA_NAME = "formulas"
 CHROMA_PATH = "./chroma_db"
 
-DAY, MONTH, YEAR, QUARTER = initialize_date()
-df, DATA_SCHEMA, FORMULA_MAP, EMBED_MODEL, CHROMA_DB = initialize()
+FORMULA_MAP = initialize_formulas()
+df, EMBED_MODEL, CHROMA_DB = initialize()
 
 aiplatform.init(project=PROJECT_ID, location=LOCATION)
 
 # ---------------------------------------------------------------------------------------------------------
-#                                           GCP Config Functions
+#                                               Functions
 # ---------------------------------------------------------------------------------------------------------
 
 def generate_prompt(query_description, relevant_formulas):
-    prompt = f"""Given a natural language question in English about a Pandas DataFrame df, write well-documented Python code that retrieves the relevant information and returns a new DataFrame named result
+    prompt = f"""Given a natural language question in English, write a simple Python code that retrieves the relevant information from Pandas DataFrame `df` and returns a new DataFrame named `result`.
+Include `reporting id` and `account name` columns by default, only omit them if absolutely unnecessary for the output.
+Use proper column names based on the dataframe schema.
+Make sure the python code is executable.
+Utilize DATAFRAME COLUMNS and FORMULAS to write code.
 
-INSTRUCTIONS:
-\tDo not modify the original DataFrame `df`, create a temporary DataFrame named `result` to hold any calculations or manipulations.
-\tAlways return a DataFrame object even if the code results in no data, return an empty DataFrame named `result`.
-\tInclude `reporting id` and `account name` columns by default, only omit them if absolutely unnecessary for the output.
-\tDo not return irrelevant columns that would slow down code execution.
-\tUse proper column names based on the dataframe schema.
-\tUtilize DATAFRAME COLUMNS, KNOWLEDGE, and FORMULAS.
+# start DATAFRAME COLUMNS #
+reporting_id: Unique id mapped to each account
+account_name: Account name
+micro_region: Micro Region (alias: mr, MR)
+nal_cluster: NAL Cluster
+nal_id: Unique id mapped to each NAL Name
+nal_name: NAL Name
+sub_region: Sub Region (alias: region)
+segment: Account segment
+account_type: account type values: GREENFIELD A (alias GF A), GREENFIELD B (GF B), SPENDER
+exit_2023_last_7_days_net_revenue: net revenue of last 7 days of December 2023
+exit_2023_last_14_days_net_revenue: net revenue of last 14 days of December 2023
+exit_2023_last_90_days_net_revenue: net revenue of last 90 days of year 2023 from October 2023 to December 2023
+exit_2023_last_7_days_gross_revenue: gross revenue of last 7 days of December 2023
+exit_2023_last_14_days_gross_revenue: gross revenue of last 14 days of December 2023
+exit_2023_last_90_days_gross_revenue: gross revenue of last 90 days of year 2023 from October 2023 to December 2023
+current_last_7_days_net_revenue: net revenue of last 7 days
+current_last_14_days_net_revenue: net revenue of last 14 days
+current_last_90_days_net_revenue: net revenue of last 90 days
+current_last_7_days_gross_revenue: gross revenue of last 7 days
+current_last_14_days_gross_revenue: gross revenue of last 14 days
+current_last_90_days_gross_revenue: gross revenue of last 90 days
+[MONTH]_[YEAR]_net_revenue: net revenue for `MONTH` in `YEAR`. Total 24 columns (e.g january_2023_net_revenue)
+[MONTH]_[YEAR]_gross_revenue: gross revenue for `MONTH` in `YEAR`. Total 24 columns (e.g january_2023_net_revenue)
+# end DATAFRAME COLUMNS #
 
-DATAFRAME COLUMNS:
-{DATA_SCHEMA}
-
-KNOWLEDGE:
-\tCURRENT DATE: {DAY} {MONTH} {YEAR}
-\tCURRENT QUARTER: {YEAR}-{QUARTER}
-\tSTARTING DATE of the dataset is January 1 2023.
-\tENDING DATE of the dataset is December 31, 2024.
-\tQuarter Breakup:
-\t\tQuarter 1 (Q1) = Jan - Mar
-\t\tQuarter 2 (Q2) = Apr - Jun
-\t\tQuarter 3 (Q3) = Jul - Sep
-\t\tQuarter 4 (Q4) = Oct - Dec
-
-FORMULAS:
+# start FORMULAS #
 {relevant_formulas}
+# end FORMULAS #
 
 Now, write Python code without any additional text for: {query_description}"""
 
     return prompt
 
-def get_relevant_formulas(query, n_results):
-    rfs = CHROMA_DB.query(query_texts=[question], n_results=2)["documents"][0]
-    rf_text = "\n".join([f"\t{rf}: {FORMULA_MAP[rf]}" for rf in rfs])
+def get_relevant_formulas(question, n_results):
+    rfs = CHROMA_DB.query(query_texts=[question], n_results=n_results)["documents"][0]
+    rf_text = "\n".join([f"- {rf}: {FORMULA_MAP[rf]}" for rf in rfs])
     return rf_text
 
 def get_model_response(prompt):
@@ -140,10 +156,8 @@ if question := st.chat_input("Ask a question"):
     with st.chat_message("user"):
         st.markdown(question)
 
-    relevant_formulas = get_relevant_formulas(question, 2)
-
-    with st.chat_message("assistant"):
-        st.markdown(relevant_formulas)
+    relevant_formulas = get_relevant_formulas(question, N_FORMULAS)
+    st.write(relevant_formulas)
     try:
         prompt = generate_prompt(question, relevant_formulas)
         model_code = get_model_response(prompt)
@@ -153,15 +167,15 @@ if question := st.chat_input("Ask a question"):
 
             result = None
             exec(model_code, globals())
-            result_tsv = data_to_tsv(result)
+            
+            if len(result) > 20:
+                st.dataframe(data=result, use_container_width=True, height=400)
+                st.info(f"Complete output has {len(result)} rows", icon="ℹ️")
+            else:
+                st.dataframe(data=result, use_container_width=True)
 
-            with st.chat_message("assistant"):
-                if len(result) > 20:
-                    st.dataframe(data=result, use_container_width=True, height=400)
-                    st.info(f"Complete output has {len(result)} rows", icon="ℹ️")
-                else:
-                    st.dataframe(data=result, use_container_width=True)
-                st_copy_to_clipboard(text="result_tsv", before_copy_label="📋 copy data", after_copy_label="✅ copied data")
+            result_tsv = data_to_tsv(result)
+            st_copy_to_clipboard(text="result_tsv", before_copy_label="📋 copy data", after_copy_label="✅ copied data")
         except Exception as e:
             st.error(f"Trouble executing query!\n\n{e}", icon="🔍")
     except Exception as e:
